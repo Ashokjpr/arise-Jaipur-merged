f3bccf0853b019b6095be7383b030edc0653c168
Ashokjpr <meenaashokkumar260@gmail.com>
2026-03-06 19:42:50 +0530

    update  Instagram Feed

--- app/(pages)/_components/InstagramFeed.tsx
+++ app/(pages)/_components/InstagramFeed.tsx
@@ -4,12 +4,7 @@ import Script from "next/script";
 
 export default function InstagramFeed() {
   return (
-    <div className="bg-gradient-to-r from-gray-50 to-white">
-      <h2 className="text-2xl md:text-5xl text-blue-900 font-semibold text-center mb-16">
-          Instagram Feed 
-         </h2>
-      <div className="w-full flex justify-center py-10">
-      
+    <div className="bg-gradient-to-r from-gray-50 to-white">      
       {/* Load Elfsight Script */}
       <Script
         src="https://apps.elfsight.com/p/platform.js"
@@ -20,7 +15,6 @@ export default function InstagramFeed() {
       {/* Instagram Feed Widget */}
       <div className="elfsight-app-1fcd3f23-2678-4e68-b966-22b8c1a89c0b" data-elfsight-app-lazy></div>
 
-    </div>
     </div>
   );
 }
